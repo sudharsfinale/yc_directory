75ff16882d5b32a7c307a77474f5ade586113431
sudharsfinale <sudharsfinale@gmail.com>
2025-05-18 23:42:23 +0530

    Search form in progress

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import SearchForm from "../components/SearchForm";
 
 const Home = () => {
   return (
     <>
     <section className="pink_container pattern">
       <h1 className="heading">PITCH YOUR STARTUP, <br /> CONNECT WITH ENTREPRENEURS</h1>
+      <p className="sub-heading !max-w-3xl">
+        Submit Ideas, Vote on Pitches and Get Noticed in Virtual Competitions
+      </p>
+      <SearchForm />
     </section>
     </>
   );
---
+++ app/components/SearchForm.tsx
@@ -0,0 +1,11 @@
+
+import Form from "next/form"
+const SearchForm = () => {
+  return (
+    <Form action={"/"} scroll={false} className="search-form">
+        <input name="query" defaultValue={""} className="search-input" placeholder="Search Startups"/>
+    </Form>
+  )
+}
+
+export default SearchForm
